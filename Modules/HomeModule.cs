8ca89e329653edad1ea942598cc8d0fe302dbb37
Carl Egbert and Jim Klein <none@none>
2016-06-30 15:59:00 -0700

    add newgame button

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -10,15 +10,15 @@ namespace Hangman
     {
       Get["/"] = _ => View["welcome.cshtml"];
 
-      // Get["/newgame"] = _ => {
-      //   Game newGame = new Game();
-      //   return View["guess.cshtml",newGame]
-      // };
-      //
+      Post["/new_game"] = _ => {
+        Game newGame = new Game();
+        return View["hangman.cshtml",newGame];
+      };
+
       // Post["/guess"] = _ => {
       //
       // };
-      // 
+      //
       //
       // }
     }
